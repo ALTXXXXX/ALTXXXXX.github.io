5ef8a49ee6839311e80a6e872382208cf6512e9e
ALTXXXXX <1078369700@qq.com>
2024-11-17 22:16:13 +0800

    Site updated: 2024-11-17 22:16:12

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731852709408";
+const VERSION = "1731852970084";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
